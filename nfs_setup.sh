1dbdba016e816d3a3ee42cc01471d07ed32a67df
Nabil DJIDI <public@djidi.com>
2016-10-18 11:41:11 +0100

    bug fix

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -7,8 +7,8 @@ mounts="${@}"
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  if (more test| grep "$src *"); then
-    echo'exist'
+  if (more /etc/exports | grep "$src *"); then
+    echo exist
   else
     echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
   fi
