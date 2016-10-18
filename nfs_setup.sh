f182b7da70ffbafc3cc30848397494073f0df034
Nabil DJIDI <public@djidi.com>
2016-10-18 12:24:24 +0100

    bug fix + suppression service getty + modification lancement service nfs

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -7,11 +7,11 @@ mounts="${@}"
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  if (more /etc/exports | grep "$src *"); then
+  if (more /etc/exports | grep "$src \*"); then
     echo exist
   else
     echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
   fi
 done
 
-exec runsvdir /etc/sv
+exec runsv /etc/sv/nfs
